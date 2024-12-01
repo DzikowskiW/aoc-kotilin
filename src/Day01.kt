afcb2a93b1c01e12dc02365820c8fe99eb354517
Wojciech Dzikowski <dzikowskw@gmail.com>
2024-12-01 15:50:22 +0100

    AoC 2024 day 1 part 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -9,15 +9,24 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val rows = input.map { row -> row.split("   ").map{ it.toInt() } }
+        val left = rows.map { it[0] }.sorted()
+        val right = rows.map { it[1] }.sorted()
+        val freqMap = right.fold(HashMap<Int, Int>()) { map, num ->
+            map[num] = map.getOrDefault(num, 0) + 1
+            map
+        }
+        return left.fold(0) { sum, num ->
+            sum + num * freqMap.getOrDefault(num, 0)
+        }
     }
 
     val testInput = readInput("Day01_test")
-    part1(testInput).println()
     check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }
