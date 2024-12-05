04231ea6aaa1c5699c03e6b21eb268ce0f017073
Wojciech Dzikowski <dzikowskw@gmail.com>
2024-12-05 22:53:20 +0100

    AoC 2024 Day 5 both parts idiomatic

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,53 +1,52 @@
 fun main() {
 
-
-    fun part1(input: List<String>):Int  {
-        val rulesGraph = hashMapOf<String, MutableList<String>>()
-
-        fun checkIfValid(pageNums:List<String>): Boolean {
-            for (i in 1..<pageNums.size) {
-                if (rulesGraph[pageNums[i-1]]?.contains(pageNums[i]) != true) {
-//                    println("fail ${pageNums[i-1]} does not contain ${pageNums[i]}")
-                    return false
-                }
+    fun bothParts(input: List<String>):Pair<Int, Int> {
+        // create rulebook
+        val rulesGraph = input
+            .subList(0, input.indexOf(""))
+            .map {row -> row.split("|") }
+            .fold(hashMapOf<String, MutableList<String>>()) { rules, (left, right) ->
+                rules.getOrPut(left) { mutableListOf() }.add(right)
+                rules
             }
-            return true
+
+        // helpers
+        fun checkIfValid(pages:List<String>) = pages.withIndex().all { (i, page) ->
+            when (i) {
+                0 -> true
+                else -> rulesGraph[pages[i-1]]?.contains(page) == true
+            }
         }
 
-        // read rules
-        var i = 0
-        while (input[i] != "") {
-            val row = input[i]
-            val (left, right) = row.split("|")
-            rulesGraph.getOrPut(left) { mutableListOf() }.add(right)
-            i += 1
+        fun reorder(pages: List<String>): List<String>  {
+            val newOrder = mutableListOf(pages[0])
+            for (i in 1..<pages.size) {
+                var added = false
+                for (j in newOrder.indices) {
+                    if (rulesGraph[pages[i]]?.contains(newOrder[j]) == true) {
+                        newOrder.add(j, pages[i])
+                        added = true
+                        break
+                    }
+                }
+                if (!added) newOrder.add(pages[i])
+            }
+            return newOrder.toList()
         }
-        rulesGraph.println()
 
         //check pages
-        i += 1
-        var result = 0
-        while (i < input.size) {
-            val pages = input[i].split(",")
-            if (checkIfValid(pages)){
-                result += pages[pages.size / 2].toInt()
+        return input.subList(input.indexOf("") + 1, input.size)
+            .map { row -> row.split(",")}
+            .fold(Pair(0,0)) { res, pages ->
+                if (checkIfValid(pages)) Pair(res.first + pages[pages.size / 2].toInt(), res.second)
+                else Pair(res.first, res.second + reorder(pages)[pages.size / 2].toInt())
             }
-            i += 1
-        }
-        return result
-    }
 
-    fun part2(input: List<String>):Int {
-        return 0
     }
 
     val testInput = readInput("Day05_test")
-
-//    check(part1(testInput) == 143)
-//    part1(testInput).println()
-//    check(part2(testInput) == 9)
+    check(bothParts(testInput) == Pair(143,123))
 
     val input = readInput("Day05")
-        part1(input).println()
-//        part2(input).println()
+    bothParts(input).println()
 }
