e7885d1776792a8e47b942224639433914cdb052
Wojciech Dzikowski <dzikowskw@gmail.com>
2024-12-07 13:15:19 +0100

    AoC 2024 Day 7 both parts

--- src/Day07.kt
+++ src/Day07.kt
@@ -4,30 +4,37 @@ fun main() {
     fun findEvals(nums:List<Long>,result:Long, acc:Long, i:Int):Int{
         if (acc > result ) return 0
         if (i >= nums.size) {
-//            println(result)
             return if (result == acc) 1 else 0
         }
-        return findEvals(nums, result, acc*nums[i], i + 1) + findEvals(nums, result, acc+nums[i], i + 1)
+        return findEvals(nums, result, acc*nums[i], i + 1) +
+                findEvals(nums, result, acc+nums[i], i + 1)
     }
 
-    fun part1(input: List<String>):Long = input.fold(0L) { acc, row ->
+    fun findEvals2(nums:List<Long>,result:Long, acc:Long, i:Int):Int{
+        if (acc > result ) return 0
+        if (i >= nums.size) {
+            return if (result == acc) 1 else 0
+        }
+        return findEvals2(nums, result, acc*nums[i], i + 1) +
+                findEvals2(nums, result, acc+nums[i], i + 1) +
+                findEvals2(nums, result, (acc.toString()+nums[i].toString()).toLong(), i + 1)
+    }
+
+    fun bothParts(input: List<String>):Pair<Long, Long> = input.fold(Pair(0L,0L)) { acc, row ->
         val (resStr, numsStr) = row.split(": ")
         val res = resStr.toLong()
         val nums = numsStr.split(" ").map { it.toLong() }
-        val a = findEvals(nums, res, nums[0], 1)
-//        println("$res | $a | $nums")
-        if (a > 0) acc + res else acc
-    }
-
-    fun part2(input: List<String>):Int {
-        return 0
+        val part1 = findEvals(nums, res, nums[0], 1)
+        val part2 = findEvals2(nums, res, nums[0], 1)
+        Pair(
+            if (part1 > 0) acc.first + res else acc.first,
+            if (part2 > 0) acc.second + res else acc.second
+        )
     }
 
     val testInput = readInput("Day07_test")
-    part1(testInput).println()
-//    part2(testInput).println()
+    bothParts(testInput).println()
 
     val input = readInput("Day07")
-    part1(input).println()
-//    part2(input).println()
+    bothParts(input).println()
 }
