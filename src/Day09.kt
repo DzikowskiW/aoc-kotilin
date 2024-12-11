3099032d57b9c333b51738072fdecbce95630ffe
Wojciech Dzikowski <dzikowskw@gmail.com>
2024-12-11 06:58:05 +0100

    AoC 2024 Day 9 part 2 wip

--- src/Day09.kt
+++ src/Day09.kt
@@ -28,15 +28,60 @@ fun main() {
             if (disk[i] == ".") break
             count +=  i * disk[i].toInt()
         }
-//        disk.println()
+        return count
+    }
+
+    fun part2(input: List<String>): Long {
+        val disk = mutableListOf<Int>()
+        var digit = 0
+        val diskSizes= hashMapOf<Int,Int>()
+        val freeSpaces = mutableListOf<Pair<Int, Int>>()
+        input[0].map { el -> el.toString().toInt()}.fold(false) { free, el ->
+            val c = if (free) -1 else digit
+            if (!free) {
+                diskSizes[c] = el
+                digit += 1
+            } else {
+                freeSpaces.add(Pair(disk.size, el))
+            }
+            for (j in 0..<el) {
+                disk.add(c)
+            }
+            !free
+        }
+
+        for (n in digit-1 downTo 0) {
+            val nSize = diskSizes[n]!!
+            var curFreeSpace = 0
+            for (j in disk.indices) {
+                if (disk[j] == n) break
+                if (disk[j] == -1) curFreeSpace += 1
+                if (disk[j+1] != -1 ) curFreeSpace = 0
+                if (curFreeSpace >= nSize) {
+                    val neww = j - curFreeSpace + 1
+                    val old = disk.first { it == n }
+                    for (k in 0..<curFreeSpace) {
+                        disk[neww+k] = n
+                        disk[old] = -1
+                    }
+                }
+            }
+        }
+
+        var count = 0L
+        for (i in disk.indices) {
+            if (disk[i] != -1)
+            count +=  i * disk[i]
+        }
+        disk.println()
         return count
     }
 
     val testInput = readInput("Day09_test")
-    part1(testInput).println()
+//    part1(testInput).println()
+    part2(testInput).println()
 
     val input = readInput("Day09")
-    // 1351621719 to low
-    // 5646589015
-    part1(input).println()
+    // part1(input).println()
+    // part2(input).println()
 }
