03a27180883c04df3875a4535af12351a1e9fdf2
Wojciech Dzikowski <dzikowskw@gmail.com>
2024-12-12 22:32:36 +0100

    AoC 2024 Day 12 part 1

--- src/Day12.kt
+++ src/Day12.kt
@@ -1,47 +1,55 @@
 fun main() {
     fun solve(input: List<String>): Any{
-        var nxt = 0L
-        var area = hashMapOf<Long, Long>()
-        var perimeter = hashMapOf<Long, Long>()
-        var cs = mutableSetOf<Long>()
-        var sums = hashMapOf<Long, Long>()
-        var areas = hashMapOf<Pair<Int,Int>, Long>()
-       input.withIndex().forEach { (y,row) ->
-           row.withIndex().forEach { (x, c) ->
-               var a = nxt
-               var p = 4
-               if (y > 0 && input[y-1][x] == c) {
-                   p -= 2
-                   a = areas.get(Pair(y-1,x))!!
-               }
-               if (x > 0 && input[y][x-1] == c){
-                   p -= 2
-                   if (p ==0) {
-                       assert(areas.get(Pair(y-1,x))!! == areas.get(Pair(y,x-1))!!)
-                   }
-                   a = areas.get(Pair(y,x-1))!!
+        val filled = mutableSetOf<Pair<Int,Int>>()
+        fun fill(sY:Int, sX:Int): Int {
+            val checked = mutableSetOf<Pair<Int, Int>>()
+            val toCheck = mutableListOf<Pair<Int, Int>>(Pair(sY, sX))
+            var area = 0
+            var perimeter = 0
+            val sval = input[sY][sX]
+            while (toCheck.isNotEmpty()) {
+                val cur = toCheck.removeFirst()
+                val (y, x) = cur
+                if (checked.contains(cur)) continue
+                checked.add(cur)
+                filled.add(cur)
 
-               }
-               if (a == nxt) nxt += 1
-               areas[Pair(y,x)] = a
-               cs.add(a)
-               area[a] = area.getOrDefault(a,0) + 1
-               perimeter[a] = perimeter.getOrDefault(a, 0) + p
-           }
-       }
-        val res =  cs.fold(0L) { acc, c ->
-            sums[c] = perimeter[c]!! * area[c]!!
-            acc + sums[c]!!
+                area += 1
+
+                for (n in listOf(Pair(y - 1, x), Pair(y + 1, x), Pair(y, x - 1), Pair(y, x + 1))) {
+                    val (ny, nx) = n
+                    if (ny !in 0..<input.size) {
+                        perimeter += 1
+                        continue
+                    }
+                    if (nx !in 0..<input[0].length) {
+                        perimeter += 1
+                        continue
+                    }
+                    if (input[ny][nx] != sval) {
+                        perimeter += 1
+                        continue
+                    }
+                    if (checked.contains(n) || toCheck.contains(n)) continue
+                    toCheck.addLast(n)
+                }
+            }
+//            println("($sY,$sX): $area, $perimeter")
+            return area * perimeter
         }
-        area.println()
-        perimeter.println()
-        sums.println()
+
+        val res = input.withIndex().fold(0) { acc, (y, row) ->
+            acc + row.withIndex().fold(0) { acc1, (x, c) ->
+                if (!filled.contains(Pair(y,x)) ) acc1 + fill(y,x) else acc1
+            }
+        }
         return res
     }
 
     val testInput = readInput("Day12_test")
     solve(testInput).println()
 
+
     val input = readInput("Day12")
-//    solve(input).println()
+    solve(input).println()
 }
