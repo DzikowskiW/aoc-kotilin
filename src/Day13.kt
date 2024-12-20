8a7d49db06bf75bbd226411294df4b43e2156a01
Wojciech Dzikowski <dzikowskw@gmail.com>
2024-12-20 23:32:22 +0100

    AoC 2024 Day 13 both parts

--- src/Day13.kt
+++ src/Day13.kt
@@ -4,29 +4,19 @@ class Machine(val a:Pair<Long,Long>, val b:Pair<Long,Long>, val prize:Pair<Long,
     override fun toString():String {
         return "[$a, $b | $prize]"
     }
-    private val cache = hashMapOf<Pair<Long,Long>, Long>()
-    fun calcTokens(curPrize:Pair<Long,Long>):Long {
-        if (curPrize.first < 0L || curPrize.second < 0L ) {
-            return Long.MAX_VALUE
-        }
-        if (curPrize.first == 0L && curPrize.second == 0L) {
-            return 0
-        }
-        if (!cache.containsKey(curPrize)) {
-            var patha = this.calcTokens(Pair(curPrize.first - a.first, curPrize.second - a.second))
-            var pathb = this.calcTokens(Pair(curPrize.first - b.first, curPrize.second - b.second))
-            if (patha != Long.MAX_VALUE) patha += 3
-            if (pathb != Long.MAX_VALUE) pathb += 1
-            cache[curPrize] =  min(patha, pathb)
-        }
-        return cache[curPrize]!!
-    }
 
+    fun calcTokens():Long {
+        val aSmash = (prize.first * b.second - prize.second * b.first) / (a.first * b.second - a.second * b.first)
+        val bSmash = (prize.first - a.first * aSmash) / b.first
+        if (a.first * aSmash + b.first*bSmash != prize.first) return -1
+        if (a.second * aSmash + b.second*bSmash != prize.second) return -1
+        return 3 * aSmash + bSmash
+    }
 }
 
 fun main() {
 
-    fun solve(input: String, mul:Long = 1): Any{
+    fun solve(input: String, mul:Long = 0): Any{
         val machines = mutableListOf<Machine>()
         val regex=Regex("\\d+", RegexOption.MULTILINE)
         val matches = regex.findAll(input).map { it -> it.value.toLong() }.toList()
@@ -35,17 +25,14 @@ fun main() {
             machines.add(Machine(
                 Pair(matches[i], matches[i+1]),
                 Pair(matches[i+2], matches[i+3]),
-                Pair(matches[i+4]*mul, matches[i+5]*mul),
+                Pair(matches[i+4]+mul, matches[i+5]+mul),
                 ))
             i+=6
         }
         var sum = 0L
         for (m in machines) {
-            val tokens = m.calcTokens(m.prize)
-            if (tokens == Long.MAX_VALUE) "no path".println()
-            else {
-                sum += tokens
-            }
+            val tokens = m.calcTokens()
+            if (tokens != -1L)  sum += tokens
         }
         return sum
     }
@@ -54,7 +41,7 @@ fun main() {
     solve(testInput).println()
     solve(testInput, 10000000000000).println()
 
-
     val input = readInputBlob("Day13")
-//    solve(input).println()
+    solve(input).println()
+    solve(input, 10000000000000).println()
 }
