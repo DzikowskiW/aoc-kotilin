d1fbcdebbcca8af9e3e0d01921528f20e1b8d749
Wojciech Dzikowski <dzikowskw@gmail.com>
2024-12-21 21:46:53 +0100

    AoC 2024 Day 21 part 2 wip

--- src/Day21.kt
+++ src/Day21.kt
@@ -1,5 +1,70 @@
 import kotlin.math.min
 
+fun combineStr(s1:Collection<String>, s2:Collection<String>):MutableSet<String> {
+    val res = mutableSetOf<String>()
+    for (a in s1) {
+        for (b in s2)
+            res.add(a+b)
+    }
+    return res
+}
+
+fun findPaths(pad:List<List<Char?>>): HashMap<Pair<Char,Char>,MutableSet<String>> {
+    val maxx = pad[0].size-1
+    val maxy = pad.size-1
+
+    // map positions of buttons
+    val pos = hashMapOf<Char,Pair<Int,Int>>()
+    pad.forEachIndexed { i, row ->
+        row.forEachIndexed { j, c ->
+            if (c != null) pos[c] = Pair(i,j)
+        }
+    }
+
+    //find shortest paths between buttons
+    val paths:HashMap<Pair<Char,Char>,MutableSet<String>> = hashMapOf()
+    for (a in pos.keys) {
+        for (b in pos.keys) {
+            val ab = Pair(a,b)
+            if (a == b)
+                paths[ab] = mutableSetOf("A")
+            else {
+                // bfs to positon
+                paths[ab] = mutableSetOf()
+                val toCheck = mutableListOf(Pair(a, ""))
+                var minPathLen = Int.MAX_VALUE
+                while (toCheck.isNotEmpty()) {
+                    val (cToCheck, pathSoFar) = toCheck.removeFirst()
+                    val (y,x) = pos[cToCheck]!!
+                    val moves = listOf(
+                        Pair(Pair(y-1, x), "^"),
+                        Pair(Pair(y+1, x), "v"),
+                        Pair(Pair(y, x-1), "<"),
+                        Pair(Pair(y, x+1), ">"),
+                    )
+                    for ((coords, pathStep) in moves) {
+                        val (ny,nx) = coords
+                        //check in pounds and not null
+                        if ((ny !in 0..maxy) || (nx !in 0..maxx) || pad[ny][nx] == null) continue
+                        if (pad[ny][nx] == b) {
+                            if (minPathLen < pathSoFar.length + 1) {
+                                toCheck.clear()
+                                break
+                            }
+                            minPathLen = pathSoFar.length + 1
+                            paths[ab]!!.add(pathSoFar + pathStep + "A")
+                        } else {
+                            toCheck.addLast(Pair(pad[ny][nx]!!, pathSoFar+pathStep))
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return paths
+}
+
+
 fun main() {
 
     fun part1(): Any {
@@ -14,70 +79,22 @@ fun main() {
             listOf('<','v','>')
         )
 
-        val cache:HashMap<String,List<String>> = hashMapOf()
-        fun solve(code:String, pad:List<List<Char?>>, topLevel:Boolean=true):Pair<List<String>,Int> {
-            if (!topLevel && cache.containsKey(code)) {
-                return Pair(cache[code]!!, 0)
-            }
-            val maxx = pad[0].size-1
-            val maxy = pad.size-1
+        val numpadPaths = findPaths(numpad)
+        val dirpadPaths = findPaths(dirpad)
 
-            // map positions of buttons
-            val pos = hashMapOf<Char,Pair<Int,Int>>()
-            pad.forEachIndexed { i, row ->
-                row.forEachIndexed { j, c ->
-                    if (c != null) pos[c] = Pair(i,j)
-                }
-            }
+        val cache:HashMap<Pair<Pair<Char,String>,Int>, List<String>> = hashMapOf()
 
-            //find shortest paths between buttons
-            val paths:HashMap<Pair<Char,Char>,MutableSet<String>> = hashMapOf()
-            for (a in pos.keys) {
-                for (b in pos.keys) {
-                    val ab = Pair(a,b)
-                    if (a == b)
-                        paths[ab] = mutableSetOf("A")
-                    else {
-                        // bfs to positon
-                        paths[ab] = mutableSetOf()
-                        val toCheck = mutableListOf(Pair(a, ""))
-                        var minPathLen = Int.MAX_VALUE
-                        while (toCheck.isNotEmpty()) {
-                            val (cToCheck, pathSoFar) = toCheck.removeFirst()
-                            val (y,x) = pos[cToCheck]!!
-                            val moves = listOf(
-                                Pair(Pair(y-1, x), "^"),
-                                Pair(Pair(y+1, x), "v"),
-                                Pair(Pair(y, x-1), "<"),
-                                Pair(Pair(y, x+1), ">"),
-                            )
-                            for ((coords, pathStep) in moves) {
-                                val (ny,nx) = coords
-                                //check in pounds and not null
-                                if ((ny !in 0..maxy) || (nx !in 0..maxx) || pad[ny][nx] == null) continue
-                                if (pad[ny][nx] == b) {
-                                    if (minPathLen < pathSoFar.length + 1) {
-                                        toCheck.clear()
-                                        break
-                                    }
-                                    minPathLen = pathSoFar.length + 1
-                                    paths[ab]!!.add(pathSoFar + pathStep + "A")
-                                } else {
-                                    toCheck.addLast(Pair(pad[ny][nx]!!, pathSoFar+pathStep))
-                                }
-                            }
-                        }
-                    }
-                }
+        fun solveDirpad(code:String, level:Int, maxLevel:Int = 2, startC:Char = 'A'):List<String> {
+            if (cache.containsKey(Pair(Pair(startC, code),level))) {
+                return cache[Pair(Pair(startC, code),level)]!!
             }
-
             //find code positions
             var seqs = mutableListOf<String>()
             for (j in 0..<code.length) {
-                val c1 = if (j ==0) 'A' else code[j-1]
+                val c1 = if (j ==0) startC else code[j-1]
                 val c2 = code[j]
                 val newSeqs = mutableListOf<String>()
-                for (p in paths[Pair(c1, c2)]!!) {
+                for (p in dirpadPaths[Pair(c1, c2)]!!) {
                     if (j == 0) {
                         newSeqs.add(p)
                         continue
@@ -88,29 +105,58 @@ fun main() {
                 }
                 seqs = newSeqs
             }
-            if (topLevel) {
-                val lev1 = seqs.flatMap { s ->
-                    val (ss, _) = solve(s, dirpad, false)
-                    ss
-                }
-                val lev2 = lev1.flatMap { s ->
-                    val (ss, _) =solve(s, dirpad, false)
-                    ss
-                }
-
-                val shortest = lev2.fold(Int.MAX_VALUE) { acc,st -> min(acc,st.length) }
-                return Pair(seqs, shortest)
+            if (level == maxLevel) {
+                cache[Pair(Pair(startC,code),level)] = seqs
+                return seqs
             }
-            cache[code] = seqs
-            return Pair(seqs, 0)
+            cache[Pair(Pair(startC, code),level)] = seqs.flatMap { s ->
+                val r1 = s.withIndex().fold(mutableSetOf<String>("")) { acc, (i,c) ->
+                    val startCC = if (i == 0) 'A' else s[i-1]
+                    combineStr(acc, solveDirpad(c.toString(), level + 1, maxLevel, startCC))
+                }
+//                r1.println()
+//                val res = solveDirpad(s, level + 1, maxLevel)
+                r1
+            }
+            return cache[Pair(Pair(startC,code),level)]!!
         }
 
+        fun solveNumpad(code:String):Int {
+            //find code positions
+            var seqs = mutableListOf<String>()
+            for (j in 0..<code.length) {
+                val c1 = if (j ==0) 'A' else code[j-1]
+                val c2 = code[j]
+                val newSeqs = mutableListOf<String>()
+                for (p in numpadPaths[Pair(c1, c2)]!!) {
+                    if (j == 0) {
+                        newSeqs.add(p)
+                        continue
+                    }
+                    for (s in seqs) {
+                        newSeqs.add(s+p)
+                    }
+                }
+                seqs = newSeqs
+            }
+
+            return seqs.flatMap { s ->
+                val r1 = s.withIndex().fold(mutableSetOf<String>("")) { acc, (i,c) ->
+                    val startCC = if (i == 0) 'A' else s[i-1]
+                    combineStr(acc, solveDirpad(c.toString(), 1, 3, startCC))
+                }
+                r1
+            }.fold(Int.MAX_VALUE) { acc,st -> min(acc,st.length) }
+        }
+
+        val testNums = listOf(
+            Pair("029A", 29),
+            Pair("980A",980),
+            Pair("179A",179),
+            Pair("456A",456),
+            Pair("379A",379),
+        )
         val numsToEnter = listOf(
-//            Pair("029A", 29),
-//            Pair("980A",980),
-//            Pair("179A",179),
-//            Pair("456A",456),
-//            Pair("379A",379),
             Pair("789A",789),
             Pair("540A",540),
             Pair("285A",285),
@@ -118,8 +164,8 @@ fun main() {
             Pair("189A",189),
         )
         var acc = 0L
-        for (numToEnter in numsToEnter) {
-            val (_, res) = solve(numToEnter.first, numpad)
+        for (numToEnter in testNums) {
+            val res = solveNumpad(numToEnter.first)
             Pair(res, numToEnter.second).println()
             acc += res * numToEnter.second
         }
