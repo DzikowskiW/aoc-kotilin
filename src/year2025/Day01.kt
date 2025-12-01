9aae0d28507bdbe7fa3284e5448eb547c45a71f8
Wojciech Dzikowski <dzikowskw@gmail.com>
2025-12-01 09:01:37 +0100

    AoC 2025 Day 1

--- src/year2025/Day01.kt
+++ src/year2025/Day01.kt
@@ -3,29 +3,54 @@ package year2025
 import println
 import readInput
 
+fun mod(a: Int, b: Int): Int {
+    return ((a % b) + b) % b
+}
+
 fun main() {
 
-    fun part1(input: String) = Regex("mul\\((\\d\\d?\\d?),(\\d\\d?\\d?)\\)")
-        .findAll(input)
-        .fold(0) { sum, el ->
-            sum + el.groupValues[1].toInt() * el.groupValues[2].toInt()
+    fun part1(input: List<String>): Int {
+        var res = 0
+        val codes = input.map {
+            Pair(it.first(), it.drop(1).toInt())
         }
+        codes.fold(50) { acc, (dir, v) ->
+//            println("acc: $acc, dir: $dir, v: $v")
+            val delta = (if (dir == 'L') -1 else 1)
+            val next = mod(acc + delta * v, 100)
+//            println("next: $next")
+            if (next == 0) res+=1
+            next
+        }
+        return res
+    }
 
-    fun part2(input: String) = Regex("mul\\((\\d\\d?\\d?),(\\d\\d?\\d?)\\)|do\\(\\)|don't\\(\\)")
-        .findAll(input)
-        .fold(Pair(0, true)) { (sum, enabled), el ->
-            when (el.groupValues[0]) {
-                "do()" -> Pair(sum, true)
-                "don't()" -> Pair(sum, false)
-                else -> Pair(if (enabled) sum + el.groupValues[1].toInt() * el.groupValues[2].toInt() else sum, enabled)
+    fun part2(input: List<String>): Int {
+        var res = 0
+        val codes = input.map {
+            Pair(it.first(), it.drop(1).toInt())
+        }
+        codes.fold(50) { acc, (dir, v) ->
+//            println("---------")
+//            println("acc: $acc, dir: $dir, v: $v")
+            val delta = (if (dir == 'L') -1 else 1)
+            val next = mod(acc + delta * v, 100)
+            if (dir == 'L') {
+                res += (mod(100 - acc, 100) + v) / 100
+            } else {
+                res += (acc + v) / 100
             }
-        }.first
+//            println("RES $res")
+//            println("next: $next")
+            mod(next, 100)
+        }
+        return res
+    }
+    val testInput = readInput("Day01_test")
+        check(part1(testInput) == 3)
+        check(part2(testInput) == 6)
 
-    val testInput = readInput("Day01_test").joinToString(separator = "")
-//        check(part1(testInput) == 161)
-//        check(part2(testInput) == 48)
-
-    val input = readInput("Day01").joinToString(separator = "")
-//        part1(input).println()
-//        part2(input).println()
+    val input = readInput("Day01")
+        part1(input).println()
+        part2(input).println()
 }
