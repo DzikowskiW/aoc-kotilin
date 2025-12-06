7a9124f31f4c13db1916868e6c2105669e01439d
Wojciech Dzikowski <dzikowskw@gmail.com>
2025-12-06 07:27:51 +0100

    AoC 2025 Day 5 part 2

--- src/year2025/Day06.kt
+++ src/year2025/Day06.kt
@@ -32,13 +32,58 @@ fun main() {
         }
     }
 
+    fun calc(op: Char, nums: List<Long>): Long {
+        if (op == '*') {
+            return nums.fold(1L) { acc, num -> acc * num }
+        }
+        else if (op == '+') {
+            return nums.fold(0L) { acc, num -> acc + num }
+        }
+        check(false)
+        return 0L
+    }
+
+    fun part2(input: List<String>): Long {
+
+        val columns = input[0].indices.map { col ->
+            input.map { line ->
+                var res = ' '
+                if (col < line.length) res = line[col]
+                res
+            }
+        }.map { it.joinToString("") }
+
+        var sum = 0L
+        var op = ' '
+        var nums = mutableListOf<Long>()
+        columns.forEach { s ->
+            var num: Long
+            if (s.trim().isNotEmpty()) {
+                if (arrayOf('*','+').contains(s.last())) {
+                    op = s.last()
+                    num = s.dropLast(1).trim().toLong()
+                } else {
+                    num = s.trim().toLong()
+                }
+                nums.add(num)
+            }
+            else {
+                sum += calc(op, nums)
+                nums = mutableListOf()
+                op = ' '
+            }
+        }
+        sum += calc(op, nums)
+
+        return sum
+    }
 
 
     val testInput = readInput("Day06_test")
         part1(testInput).println()
-//        check(part2(testInput) == 14L)
+        part2(testInput).println()
 
     val input = readInput("Day06")
         part1(input).println()
-//        part2(input).println()
+        part2(input).println()
 }
